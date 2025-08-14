0c2a0945790ffee32fa76537ed0db25f3be67de1
gian-pradipta <93311140+gian-pradipta@users.noreply.github.com>
2025-08-15 00:00:10 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ const tap = document.getElementById("tap-1");
 const s2 = document.getElementById("scene-2");
 const s3 = document.getElementById("scene-3");
 const scenes = [tap, s2, s3];
+const text = `
+Semoga dengan bertambahnya usiamu, semakin sempurna juga imanmu, makin rajin ibadahnya, selalu diberi kesehatan oleh Yang Maha Kuasa, dipanjangkan umurnya, selalu bahagia, selalu berbakti kepada orang tua, dan semoga segala doa serta cita-cita yang kamu inginkan bisa terwujud dengan mudah. Aamiin.
+<
+Hari ini hari spesial kamu, sayang. Sekarang kamu semakin dewasa, yang pastinya udah tahu mana yang baik dan nggak baik buat dirimu. Aku harap kamu bisa lebih bijak dan dewasa dari sebelumnya, terus semangat untuk memperbaiki diri jadi pribadi yang lebih baik lagi. Jangan pernah lelah mengejar semua impian kamu, karena aku percaya kamu pasti bisa.
+<
+Jangan takut gagal ya, sayang. Selama aku ada, aku bakal selalu dukung kamu dan support kamu sepenuh hati. Tetap rendah hati, jadi anak yang baik, dan selalu sayang sama keluargamu. Jangan lupa untuk selalu bersyukur atas setiap hal kecil maupun besar yang kamu terima.
+<
+Hadiah dari aku memang cuma boneka sederhana, tapi ada artinya. Boneka ini aku titipin rasa sayangku. Biar saat kamu peluk dia, kamu ngerasa aku ada di situ, nemenin kamu. Walaupun aku nggak bisa selalu ada secara fisik, boneka ini jadi pengingat kalau ada seseorang di sini yang peduli dan sayang sama kamu tanpa syarat.
+<
+Walaupun kita terhalang jarak, percayalah… doaku nggak pernah jauh darimu. Aku selalu minta sama Allah supaya kamu selalu diberi kelancaran, keselamatan, dan dijaga dalam setiap langkahmu.
+<
+Semoga di ulang tahun berikutnya, aku bisa ada di sisimu, merayakan bareng, dan melihat senyum kamu dari dekat. Aamiin.
+<
+Selamat ulang tahun sekali lagi, sayangku. Semoga semua yang kamu lakukan hari ini, besok, dan seterusnya selalu dimudahkan dan diberkahi. 💙
+`
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -28,8 +43,10 @@ function typeText(element, text, delay = 100) {
     let index = 0;
     async function type() {
         if (index < text.length) {
-            element.textContent += text.charAt(index);
+            const char = text.charAt(index) == "<" ? "<br>" : text.charAt(index);
+            element.innerHTML += char;
             index++;
+            element.scrollTop = element.scrollHeight;
             await sleep(delay)
             setTimeout(type, delay);
         }
@@ -54,7 +71,7 @@ function main() {
                     await sleep(2000);
                     s3.style.backgroundImage = "url('./confetti.gif')";
                     await sleep(100);
-                    typeText(t, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, dolor et itaque expedita similique tempora rerum. Blanditiis numquam nemo corporis officiis voluptatibus pariatur est facere minus assumenda consequuntur, porro saepe ipsum, earum id voluptas quis et delectus deleniti temporibus ipsam ratione soluta optio voluptatum. Temporibus dolor aliquid earum blanditiis quis cupiditate tempore placeat dolore? Necessitatibus, rem dolore velit suscipit impedit quibusdam placeat voluptates rerum. Explicabo voluptates iusto tempore hic libero voluptas saepe doloremque consequatur, accusantium aliquid aut fugiat, obcaecati modi, totam odit? Ut, recusandae modi iste tempore in molestiae ratione omnis sed nisi quod explicabo natus libero. Pariatur, illo voluptas!", 20);
+                    typeText(t, text, 20);
                 }
             }
         });
